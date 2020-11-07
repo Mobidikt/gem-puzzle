a6b99620f609191d58ed99f751c422972f157867
Kirill Mecker <ghost3299@yandex.ru>
2020-11-07 16:48:25 +0400

    feat: move gem

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ const empty ={
 };
 
 const cells = [];
+const moves = 0;
 
 cells.push(empty);
 
 function move(index) {
     const cell = cells[index];
+    const leftDiff = Math.abs(empty.left-cell.left);
+    const leftTop = Math.abs(empty.top-cell.top);
+
+    if(leftDiff+leftTop> 1){
+        return;
+    }
+
     cell.element.style.top= `${empty.top * cellSize}px`;
     cell.element.style.left= `${empty.left * cellSize}px`;
 
@@ -20,17 +28,29 @@ function move(index) {
     empty.top=cell.top;
     cell.top=emptyTop;
     cell.left=emptyLeft;
+    moves = moves+1;
+    const isFinished = cells.every(cell => {
+        return cell.value === cell.top * 4 +cell.left;
+    })
+    if(isFinished) {
+        alert('You won');
+    }
 }
 
+const numbers = [...Array(15).keys()].sort(()=>Math.random()-0.5);
+
 for ( let i=1; i<=15; i++){
+    console.log(numbers);
+    const value = numbers[i-1]+1;
     const cell = document.createElement('div');
     cell.className= 'cell';
-    cell.innerHTML= i;
+    cell.textContent= value;
 
     const left = i% 4;
     const top = (i-left) /4;
 
     cells.push({
+        value: value,
         left: left,
         top: top,
         element: cell
